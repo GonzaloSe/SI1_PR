b1856da78dda79fa3b863ba5ac808d8569fcd3cd
Iker González Sánchez <101722105+ikergs03@users.noreply.github.com>
2023-11-07 23:40:32 +0100

    Update actualiza.sql

--- p2/sql_files/actualiza.sql
+++ p2/sql_files/actualiza.sql
@@ -1,27 +1,87 @@
--- Agregar una restricción UNIQUE a la columna 'username' en la tabla 'customers'
-ALTER TABLE customers
-ADD CONSTRAINT uk_username UNIQUE (username);
-
--- Agregar una clave foránea a la tabla 'orders' que referencia la tabla 'customers'
-ALTER TABLE orders
-ADD CONSTRAINT fk_customer FOREIGN KEY (customerid) REFERENCES customers(customerid);
-
--- Configurar la clave foránea para actualizar en cascada
+-- Agregar una clave foránea a la tabla 'orders' que referencia la tabla 'customers' y configurarla para actualizar en cascada
 ALTER TABLE orders
 ADD CONSTRAINT fk_customer FOREIGN KEY (customerid) REFERENCES customers(customerid) ON DELETE CASCADE;
 
-
+-- Agregar una restricción UNIQUE a la columna 'username' en la tabla 'customers'
 -- Agregar un campo 'balance' en la tabla 'customers'
+-- Aumentar el tamaño del campo 'password' en la tabla 'customers'
 ALTER TABLE customers
-ADD balance NUMERIC(10, 2); -- Ajusta el tipo y la precisión según tus necesidades
-
+ADD balance NUMERIC(10, 2), -- Ajusta el tipo y la precisión según tus necesidades
+ALTER COLUMN password TYPE VARCHAR(96); -- Ajusta el tipo y la longitud según tus necesidades
 
 -- Crear una nueva tabla 'ratings' para guardar las valoraciones
 CREATE TABLE ratings (
   rating_id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES customers(customerid),
-  movie_id INTEGER REFERENCES imdb_movies(movie_id),
+  movie_id INTEGER REFERENCES imdb_movies(movieid),
   rating INTEGER CHECK (rating >= 1 AND rating <= 5),
   UNIQUE (user_id, movie_id) -- Evita que un usuario valore la misma película dos veces
 );
 
+
+-- Agregar dos campos a la tabla 'imdb_movies' para contener la valoración media y el número de valoraciones
+ALTER TABLE imdb_movies
+ADD ratingmean NUMERIC(3, 2), -- Ajusta el tipo y la precisión según tus necesidades
+ADD ratingcount INTEGER;
+
+
+
+-------------------------------------------------TRIGGERS--------------------------------------------------------------------
+-- Crear un trigger para actualizar el saldo del cliente cuando se inserta una nueva orden
+CREATE OR REPLACE FUNCTION update_balance_on_order_insert()
+RETURNS TRIGGER AS $$
+BEGIN
+  -- Lógica para actualizar el saldo, por ejemplo, deducir el costo de la orden
+  -- Puedes personalizar esta lógica según tus necesidades
+  UPDATE customers
+  SET balance = balance - NEW.order_amount
+  WHERE customerid = NEW.customerid;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_balance_trigger
+AFTER INSERT ON orders
+FOR EACH ROW
+EXECUTE FUNCTION update_balance_on_order_insert();
+
+-- Crear un trigger para calcular y actualizar la valoración media de una película cuando se inserta una nueva valoración
+CREATE OR REPLACE FUNCTION update_ratingmean_on_rating_insert()
+RETURNS TRIGGER AS $$
+BEGIN
+  -- Lógica para calcular la valoración media, por ejemplo, promediar las valoraciones de una película
+  -- Puedes personalizar esta lógica según tus necesidades
+  UPDATE imdb_movies
+  SET ratingmean = (SELECT AVG(rating) FROM ratings WHERE movie_id = NEW.movie_id)
+  WHERE movie_id = NEW.movie_id;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_ratingmean_trigger
+AFTER INSERT ON ratings
+FOR EACH ROW
+EXECUTE FUNCTION update_ratingmean_on_rating_insert();
+
+
+
+-------------------------------------------------PROCEDIMIENTOS--------------------------------------------------------------------
+-- Crear un procedimiento para inicializar el campo 'balance' de 'customers' con un valor aleatorio
+CREATE OR REPLACE FUNCTION setCustomersBalance(IN initialBalance bigint)
+RETURNS VOID AS $$
+DECLARE
+    randomBalance bigint;
+BEGIN
+    -- Generar un número aleatorio entre 0 y N (en este caso, N = 200)
+    randomBalance := floor(random() * (initialBalance + 1));
+    
+    -- Actualizar el campo 'balance' para todos los registros en la tabla 'customers' con el valor aleatorio
+    UPDATE customers
+    SET balance = randomBalance;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+-- Llamar al procedimiento para inicializar el campo 'balance' de 'customers' con un número aleatorio entre 0 y 200
+SELECT setCustomersBalance(200);
