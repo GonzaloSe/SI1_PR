ba6bf67a7e2a328e7bc39e1043923bcbae91c8f9
cagadon de gonzalo <iker.gonzalezs@estudiante.uam.es>
2023-11-17 12:30:47 +0100

    python

--- p2/python_files/mostrarTabla.py
+++ p2/python_files/mostrarTabla.py
@@ -1,19 +1,24 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text, func
 
 
 def main():
     # engine is an instance of AsyncEngine
     engine = create_engine(
-        "postgresql://alumnodb:password@:::5432/si1",
+        "postgresql://alumnodb:password@localhost:5432/si1",
         echo=True,
     )
 
     conection = engine.connect()
-    
-    result = conection.execute("SELECT * FROM orders LIMIT 10")
 
-    for row in result:
-        print(row)
+    function = func.getTopSales(2022,2023)
+    
+    result = conection.execute(text(f"SELECT {function} AS resultado;"))
+
+    # Recupera el resultado
+    resultado_final = result.scalar()
+
+    # Imprime el resultado (o haz lo que necesites con él)
+    print(resultado_final)
 
     conection.close()
     engine.dispose()
