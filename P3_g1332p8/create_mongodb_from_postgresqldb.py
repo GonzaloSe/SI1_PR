8fd36fb0c77939320fb2ad6868d9fd376715f84c
Gonzalo Segovia <segoviaoblanca@gmail.com>
2023-12-01 11:26:56 +0100

    p3 inicio

--- P3_g1332p8/create_mongodb_from_postgresqldb.py
+++ P3_g1332p8/create_mongodb_from_postgresqldb.py
@@ -1,27 +1,55 @@
 import os
 import sys, traceback, time
 
-from sqlalchemy import create_engine
+
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
 from pymongo import MongoClient
 
-def get_database():
- 
-   # Provide the mongodb atlas url to connect python to mongodb using pymongo
-   CONNECTION_STRING = ""
- 
-   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-   client = MongoClient(CONNECTION_STRING)
- 
-   # Create the database for our example (we will use the same database throughout the tutorial
-   return client['si1']
-  
-# This is added so that many files can reuse the function get_database()
-if __name__ == "__main__":   
-  
-   # Get the database
-   dbname = get_database()
+# Configuración de la conexión a PostgreSQL
+postgres_username = 'alumnodb'
+postgres_password = 'alumnodb'
+postgres_host = 'localhost'
+postgres_port = '5432'
+postgres_database = 'si1'
 
-   # Get the collection
-   france_co = dbname["france"]
+# Configuración de la conexión a MongoDB
+mongodb_host = 'localhost'
+mongodb_port = 27017
+mongodb_database = 'si1'
+mongodb_collection = 'france'
 
-   
+def create_mongodb_from_postgres():
+    # Conexión a PostgreSQL
+    postgres_uri = f'postgresql://{postgres_username}:{postgres_password}@{postgres_host}:{postgres_port}/{postgres_database}'
+    postgres_engine = create_engine(postgres_uri)
+    Session = sessionmaker(bind=postgres_engine)
+    session = Session()
+
+    # Conexión a MongoDB
+    mongodb_client = MongoClient(mongodb_host, mongodb_port)
+    mongodb_db = mongodb_client[mongodb_database]
+    mongodb_col = mongodb_db[mongodb_collection]
+
+    # Obtener metadatos de las tablas en PostgreSQL
+    metadata = MetaData()
+    metadata.reflect(bind=postgres_engine)
+
+    # Iterar sobre las tablas y copiar datos a MongoDB
+    for table_name, table in metadata.tables.items():
+        # Obtener todos los registros de la tabla
+        print(f'Copiando datos de la tabla {table_name}...')
+        records = session.query(table).all()
+
+        # Insertar registros en la colección de MongoDB
+        for record in records:
+            print(f'Insertando registro {record}...')
+            document = record._asdict()
+            mongodb_col.insert_one(document)
+
+    # Cerrar conexiones
+    session.close()
+    mongodb_client.close()
+
+if __name__ == "__main__":
+    create_mongodb_from_postgres()
